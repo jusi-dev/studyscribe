4e97dfd41d8c9a4f4c24a372290fcfd4ecde4542
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2024-07-02 22:11:02 +0200

    changer

---
+++ app/footer.jsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+        <div className="w-full bg-slate-200 py-10 border-t-2 border-prim">
+            <div>
+                <p className="text-prim text-center font-bold">StudyScribe</p>
+                <p className="text-sec text-center">© 2024 StudyScribe. All rights reserved.</p>
+            </div>
+        </div>
+    )
+}
